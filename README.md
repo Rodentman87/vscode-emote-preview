75f9fc4ba6b55085ca9f0109a3929e59a3acc674
Maisy <rodentman87@gmail.com>
2022-06-10 21:45:59 -0500

    Make sure it runs on all files without needing to load into a TS or JS file first

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 ## Features
 
 This extension provides an inline preview of discord emoji inside the editor.
+
+## Gallery
+
+Emotes displayed here are from the lovely [Icons Discord](https://discord.gg/aPvvhefmt3)
+
+Useful for making sure you're setting the right emote on your buttons.
+
+![Wumpus Icon](/assets/example_wumpus.png)
+
+or custom config
+
+![Emote Config](/assets/example_config.png)
--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
 const regex = /<a?:.+?:(\d{17,})>/g;
@@ -101,17 +99,8 @@ class StringAnnotation implements vscode.Disposable {
 	}
 }
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log(
-		'Congratulations, your extension "vscode-emote-inlay" is now active!'
-	);
-
 	context.subscriptions.push(new StringAnnotation());
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
